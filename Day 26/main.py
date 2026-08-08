ccae7ef62524a37bbc136b60f73ae739a24f1a8e
Harshita <raghavharshita999@gmail.com>
2026-08-08 15:35:03 +0530

    Error handling feature added

--- Day 26/main.py
+++ Day 26/main.py
@@ -3,8 +3,14 @@ import pandas as pd
 data = pd.read_csv("nato_phonetic_alphabet.csv")
 code_dict = {row.Letter:row.Code for index,row in data.iterrows()}
 
-word = input("Enter a word: ")
+def generate_phonetic():
+    word = input("Enter a word: ")
+    try:
+        some = [code_dict[f"{i.upper()}"] for char in word.split() for i in char]
+    except KeyError:
+        print("Sorry, only letters allowed")
+        generate_phonetic()
+    else:
+        print(some)
 
-some = [code_dict[f"{i.upper()}"] for char in word.split() for i in char]
-
-print(some)
+generate_phonetic()
